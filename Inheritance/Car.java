3d5bf9fda257c85e7e456574d28163f06e7e87e9
Sudipta Roy <75389717+sudiptaaaa@users.noreply.github.com>
2023-01-31 12:26:28 +0530

    files added

--- Inheritance/Car.java
+++ Inheritance/Car.java
@@ -1,5 +1,24 @@
 package Inheritance;
 
-public class Car {
-    
+public class Car extends Vehicle{
+    private boolean isImported;
+
+    public Car(String color, int noOfWheels, String model, boolean isImported) {
+        super(color, noOfWheels, model);
+        this.isImported = isImported;
+    }
+
+    @Override
+    void amountOfSpace() {
+        System.out.println("Car has the least space");
+    }
+
+    @Override
+    public String toString() {
+        String det = super.toString();
+        return "Car{" + '\'' +
+                det +
+                ", isImported=" + isImported +
+                '}';
+    }
 }
