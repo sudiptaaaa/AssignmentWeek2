8398642f8a2ae99249df1099253ecdc37c1311ec
Sudipta Roy <75389717+sudiptaaaa@users.noreply.github.com>
2023-01-31 12:25:52 +0530

    files added

--- Inheritance/Bus.java
+++ Inheritance/Bus.java
@@ -1,5 +1,24 @@
 package Inheritance;
 
-public class Bus {
-    
+public class Bus extends Vehicle{
+    private boolean isPublicTransport;
+
+    public Bus(String color, int noOfWheels, String model, boolean isPublicTransport) {
+        super(color, noOfWheels, model);
+        this.isPublicTransport = isPublicTransport;
+    }
+
+    @Override
+    void amountOfSpace() {
+        System.out.println("Bus has the most space");
+    }
+
+    @Override
+    public String toString() {
+        String det = super.toString();
+        return "Bus{" +  '\'' +
+                det +
+                ", isPublicTransport=" + isPublicTransport +
+                '}';
+    }
 }
