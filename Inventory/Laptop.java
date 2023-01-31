806b669b54aca3b11cf0335bcf77f7bb8f66a55e
Sudipta Roy <75389717+sudiptaaaa@users.noreply.github.com>
2023-01-31 12:16:51 +0530

    Update Laptop.java

--- Inventory/Laptop.java
+++ Inventory/Laptop.java
@@ -1,5 +1,22 @@
 package Inventory;
 
-public class Laptop {
-    
+public class Laptops extends Inventory{
+    private int id;
+    private String description;
+
+    public Laptops(int id) {
+        this.id = id;
+        this.description = "L"+id;
+        super.setLowOrderLevelQuantity(3);
+    }
+
+    @Override
+    public String toString() {
+        return "Laptops{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", quantity=" + quantity +
+                ", lowOrderLevelQuantity=" + lowOrderLevelQuantity +
+                '}';
+    }
 }
