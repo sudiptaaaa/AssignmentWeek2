83c4f77798d0a0bed5187f7d08c9c518ae00e8ba
Sudipta Roy <75389717+sudiptaaaa@users.noreply.github.com>
2023-01-31 12:16:10 +0530

    Update Accessories.java

--- Inventory/Accessories.java
+++ Inventory/Accessories.java
@@ -1,5 +1,22 @@
 package Inventory;
 
-public class Accessories {
-    
+public class Accessories extends Inventory{
+    private int id;
+    private String description;
+
+    public Accessories(int id) {
+        this.id = id;
+        this.description = "A"+id;
+        super.setLowOrderLevelQuantity(5);
+    }
+
+    @Override
+    public String toString() {
+        return "Accessories{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", quantity=" + quantity +
+                ", lowOrderLevelQuantity=" + lowOrderLevelQuantity +
+                '}';
+    }
 }
