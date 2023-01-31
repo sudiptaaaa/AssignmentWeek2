d586565914baff7c089ef99b9391307fe7763b2f
Sudipta Roy <75389717+sudiptaaaa@users.noreply.github.com>
2023-01-31 12:28:25 +0530

    files added

--- Inheritance/Vehicle.java
+++ Inheritance/Vehicle.java
@@ -1,5 +1,31 @@
 package Inheritance;
 
-public class Vehicle {
-    
+public abstract class Vehicle {
+    private String color;
+    private int noOfWheels;
+    private String model;
+
+
+    public Vehicle(String color, int noOfWheels, String model) {
+        this.color = color;
+        this.noOfWheels = noOfWheels;
+        this.model = model;
+        this.honks();
+        this.amountOfSpace();
+    }
+
+
+    public void honks(){
+        System.out.println(this.getClass().getSimpleName() + " honks");
+    }
+
+    abstract void amountOfSpace() ;
+
+    @Override
+    public String toString() {
+        return "color='" + color + '\'' +
+                ", noOfWheels=" + noOfWheels +
+                ", model='" + model + '\'';
+
+    }
 }
