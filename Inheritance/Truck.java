b79750aeda7dfa535a71d3a17b89775b9bd7cd23
Sudipta Roy <75389717+sudiptaaaa@users.noreply.github.com>
2023-01-31 12:27:18 +0530

    files added

--- Inheritance/Truck.java
+++ Inheritance/Truck.java
@@ -1,5 +1,25 @@
 package Inheritance;
 
-public class Truck {
-    
+public class Truck extends Vehicle{
+    private boolean isPublicTransport;
+
+    private String shippingDetails;
+    public Truck(String color, int noOfWheels, String model,String shippingDetails) {
+        super(color, noOfWheels, model);
+        this.shippingDetails = shippingDetails;
+    }
+
+    @Override
+    void amountOfSpace() {
+        System.out.println("Truck has medium amount of space");
+    }
+
+    @Override
+    public String toString() {
+        String det = super.toString();
+        return "Truck{" +  '\'' +
+                det +
+                ", shippingDetails='" + shippingDetails + '\'' +
+                '}';
+    }
 }
